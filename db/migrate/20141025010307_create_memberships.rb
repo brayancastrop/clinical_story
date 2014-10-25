b4e978262e323f1d4a8f7ac13f1dee4b105dd261
Prelang Builder <builder@prelang.com>
2014-10-25 01:03:07 +0000

    Generated model Membership with migration

---
+++ db/migrate/20141025010307_create_memberships.rb
@@ -0,0 +1,11 @@
+class CreateMemberships < ActiveRecord::Migration
+  def change
+    create_table :memberships do |t|
+      t.references :user, index: true
+      t.references :account, index: true
+      t.references :role, index: true
+
+      t.timestamps
+    end
+  end
+end
---
+++ spec/factories/memberships.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :membership do
+    user nil
+account nil
+role nil
+  end
+
+end
